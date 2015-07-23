55c13dfa40a8c11194e2ce38d81e4aacd851ebdb
MashaKazachenok <kazachenokm@mail.ru>
2015-07-23 16:53:50 +0300

    added projects Models and Manager

---
+++ ServiceForMinibuses/ServiceForMinibuses.Web/ServiceForMinibuses.Manager.EntityFramework/EntityFrameworkRouteManager.cs
@@ -0,0 +1,8 @@
+﻿
+
+namespace ServiceForMinibuses.Manager.EntityFramework
+{
+    public class EntityFrameworkRouteManager
+    {
+    }
+}
---
+++ ServiceForMinibuses/ServiceForMinibuses.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ServiceForMinibuses.Web.Startup))]
+namespace ServiceForMinibuses.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
