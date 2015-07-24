df4babdeb56bf8b5bccbd4a89b73a3ec927a5876
MashaKazachenok <kazachenokm@mail.ru>
2015-07-24 11:39:21 +0300

    fixed test

--- ServiceForMinibuses/ServiceForMinibuses.Manager.EntityFramework.Tests/RouteStoreTests.cs
+++ ServiceForMinibuses/ServiceForMinibuses.Manager.EntityFramework.Tests/RouteStoreTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Data.Entity;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
 using Models;
 using Moq;
 using NUnit.Framework;
@@ -14,17 +16,30 @@ namespace ServiceForMinibuses.Manager.EntityFramework.Tests
         public void GetById_Found()
         {
             // Подготовка
-            var routeDbSetMock = new Mock<DbSet<Route>>();
+         /*   var routeDbSetMock = new Mock<DbSet<Route>>();
             routeDbSetMock.Setup(x => x.Find(1))
                 .Returns(new Route
                 {
                     Name = "Gomel-Minsk",
                     Id = 1
                 });
+            */
+            var routeDbSetMock = new List<Route> 
+            { 
+                new Route { Name = "Gomel-Minsk",
+                    Id = 1}, 
+           
+            }.AsQueryable(); 
+
+            var mockSet = new Mock<DbSet<Route>>();
+            mockSet.As<IQueryable<Route>>().Setup(m => m.Provider).Returns(routeDbSetMock.Provider);
+            mockSet.As<IQueryable<Route>>().Setup(m => m.Expression).Returns(routeDbSetMock.Expression);
+            mockSet.As<IQueryable<Route>>().Setup(m => m.ElementType).Returns(routeDbSetMock.ElementType);
+            mockSet.As<IQueryable<Route>>().Setup(m => m.GetEnumerator()).Returns(routeDbSetMock.GetEnumerator()); 
 
             var databaseContextMock = new Mock<IDatabaseContext>();
-            databaseContextMock.Setup(x => x.Set<Route>())
-                .Returns(routeDbSetMock.Object);
+            databaseContextMock.Setup(x => x.Routes)
+                .Returns(mockSet.Object);
 
             var routeStore = new RouteStore(databaseContextMock.Object);
 
@@ -34,7 +49,7 @@ namespace ServiceForMinibuses.Manager.EntityFramework.Tests
             // Утверждение
             Assert.IsNotNull(route);
             Assert.AreEqual(1, route.Id);
-            Assert.AreEqual("Gomel - Minsk", route.Name);
+            Assert.AreEqual("Gomel-Minsk", route.Name);
         }
     }
 }
