5268e2890d45b51e1dbc1e5c4bfe778a47f8b3c1
MashaKazachenok <kazachenokm@mail.ru>
2015-08-05 13:00:09 +0300

    added tests for RouteController

--- ServiceForMinibuses/ServiceForMinibuses.Web.Tests/RouteControllerTests.cs
+++ ServiceForMinibuses/ServiceForMinibuses.Web.Tests/RouteControllerTests.cs
@@ -1,8 +1,11 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Models;
 using Moq;
 using NUnit.Framework;
 using ServiceForMinibuses.Manager;
 using ServiceForMinibuses.Web.Controllers;
+using ServiceForMinibuses.Web.Models;
 
 
 namespace ServiceForMinibuses.Web.Tests
@@ -26,7 +29,7 @@ namespace ServiceForMinibuses.Web.Tests
           }
 
             [Test]
-            public void CreateRoute_CheckRenderView_ResultView()
+            public void Get_CreateRoute_CheckRenderView_ResultView()
             {
 
                 var routeStoreMock = new Mock<IRouteStore>();
@@ -36,10 +39,13 @@ namespace ServiceForMinibuses.Web.Tests
                 var controller = new RouteController(routeStoreMock.Object, stopStoreMock.Object);
 
                 var actionResult = controller.CreateRoute();
+                var viewResult = (ViewResult)actionResult;
+
                 Assert.IsInstanceOf<ViewResult>(actionResult);
+                Assert.IsInstanceOf<CreateRouteViewModel>(viewResult.Model);
+                Assert.AreEqual(viewResult.ViewName, string.Empty);
                
             }
-
 
             [Test]
             public void DeleteRoute()
@@ -53,9 +59,25 @@ namespace ServiceForMinibuses.Web.Tests
                 Assert.IsNotNull(actionResult);
                 Assert.IsInstanceOf<RedirectToRouteResult>(actionResult);
                 var routeResult = (RedirectToRouteResult)actionResult;
-               Assert.IsNotNull(routeResult.RouteValues);
-              Assert.AreEqual(routeResult.RouteValues["Action"], "ViewRoutes" );
+                Assert.IsNotNull(routeResult.RouteValues);
+                Assert.AreEqual(routeResult.RouteValues["Action"], "ViewRoutes" );
+            }
 
+            [Test]
+            public void ViewRoutes()
+            {
+                var routeStoreMock = new Mock<IRouteStore>();
+                var stopStoreMock = new Mock<IStopStore>();
+
+                var controller = new RouteController(routeStoreMock.Object, stopStoreMock.Object);
+
+
+                var actionResult = controller.ViewRoutes();
+                var viewResult = (ViewResult)actionResult;
+                Assert.IsInstanceOf<ViewResult>(actionResult);
+                Assert.IsInstanceOf<RouteListViewModel>(viewResult.Model);
+                Assert.AreEqual(viewResult.ViewName, string.Empty);
+               
             }
 
     }
--- ServiceForMinibuses/ServiceForMinibuses.Web/Controllers/RouteController.cs
+++ ServiceForMinibuses/ServiceForMinibuses.Web/Controllers/RouteController.cs
@@ -122,7 +122,5 @@ namespace ServiceForMinibuses.Web.Controllers
 
             return RedirectToAction("ViewRoutes");
         }
-
-     
     }
 }
