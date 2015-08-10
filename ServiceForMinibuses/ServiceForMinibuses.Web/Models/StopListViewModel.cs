c6f30ba10b47d6bf466d9ea7d0b7195f990eff6c
MashaKazachenok <kazachenokm@mail.ru>
2015-08-10 16:30:10 +0300

    delete packages

---
+++ ServiceForMinibuses/ServiceForMinibuses.Web/Models/StopListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ServiceForMinibuses.Web.Models
+{
+    public class StopListViewModel
+    {
+        public List<CreateStopViewModel> Stopes { get; set; }
+    }
+}
